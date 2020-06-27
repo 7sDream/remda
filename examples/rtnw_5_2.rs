1344f1f7ce3c74f969ffc09446d517566b87a3fc
7sDream <i@7sdre.am>
2020-06-27 18:13:48 +0800

    feat(texture/noise): add smooth perlin noise texture

--- examples/rtnw_5_2.rs
+++ examples/rtnw_5_2.rs
@@ -8,7 +8,7 @@ use remda::{
 
 fn main() {
     let mut world = GeometryList::default();
-    let perlin = Perlin::new(256, false);
+    let perlin = Perlin::new(256, true);
 
     world
         .add(Sphere::new(
@@ -34,6 +34,6 @@ fn main() {
         .take_photo(world)
         .height(300)
         .samples(128)
-        .shot(Some("rtnw_5_1.ppm"))
+        .shot(Some("rtnw_5_2.ppm"))
         .unwrap();
 }
--- src/texture/noise.rs
+++ src/texture/noise.rs
@@ -3,6 +3,7 @@ use {super::Texture, crate::prelude::*};
 #[derive(Debug, Clone)]
 pub struct Perlin {
     point_count: usize,
+    smooth: bool,
     ran_float: Vec<f64>,
     perm_x: Vec<usize>,
     perm_y: Vec<usize>,
@@ -11,7 +12,7 @@ pub struct Perlin {
 
 impl Perlin {
     #[must_use]
-    pub fn new(point_count: usize) -> Self {
+    pub fn new(point_count: usize, smooth: bool) -> Self {
         let ran_float = (0..point_count).map(|_| Random::normal()).collect();
         let mut perm_x = (0..point_count).collect();
         Random::shuffle(&mut perm_x);
@@ -21,6 +22,7 @@ impl Perlin {
         Random::shuffle(&mut perm_z);
         Self {
             point_count,
+            smooth,
             ran_float,
             perm_x,
             perm_y,
@@ -29,15 +31,59 @@ impl Perlin {
     }
 }
 
+#[allow(clippy::cast_precision_loss)] // i j k is small enough
+fn interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    (0..2)
+        .flat_map(move |i| {
+            (0..2).flat_map(move |j| {
+                (0..2).map(move |k| {
+                    (i as f64).mul_add(u, (1 - i) as f64 * (1.0 - u))
+                        * (j as f64).mul_add(v, (1 - j) as f64 * (1.0 - v))
+                        * (k as f64).mul_add(w, (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k]
+                })
+            })
+        })
+        .sum()
+}
+
 impl Texture for Perlin {
     #[allow(clippy::cast_sign_loss)] // because we do abs before cast
+    #[allow(clippy::cast_possible_wrap)] // because di dj dk and point_count is small enough
     #[allow(clippy::cast_possible_truncation)] // truncation is expected behavior
+    #[allow(clippy::cast_precision_loss)] // scene is not so big
+    #[allow(clippy::many_single_char_names)]
     fn color(&self, _u: f64, _v: f64, point: &Point3) -> Color {
-        let i = ((4.0 * point.x).abs() as usize) & (self.point_count - 1);
-        let j = ((4.0 * point.y).abs() as usize) & (self.point_count - 1);
-        let k = ((4.0 * point.z).abs() as usize) & (self.point_count - 1);
+        Color::newf(1.0, 1.0, 1.0)
+            * if self.smooth {
+                let i = point.x.floor() as isize;
+                let j = point.y.floor() as isize;
+                let k = point.z.floor() as isize;
+                let u = point.x - i as f64;
+                let v = point.y - j as f64;
+                let w = point.z - k as f64;
 
-        let radio = self.ran_float[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]];
-        Color::newf(1.0, 1.0, 1.0) * radio
+                let mut grays = [[[0.0; 2]; 2]; 2];
+
+                (0..2).for_each(|di| {
+                    (0..2).for_each(|dj| {
+                        (0..2).for_each(|dk| {
+                            let xi = ((i + di as isize) & (self.point_count - 1) as isize) as usize;
+                            let yi = ((j + dj as isize) & (self.point_count - 1) as isize) as usize;
+                            let zi = ((k + dk as isize) & (self.point_count - 1) as isize) as usize;
+                            let index = self.perm_x[xi] ^ self.perm_y[yi] ^ self.perm_z[zi];
+                            grays[di][dj][dk] = self.ran_float[index];
+                        })
+                    })
+                });
+
+                interp(grays, u, v, w)
+            } else {
+                let i = (((4.0 * point.x) as isize) & (self.point_count - 1) as isize) as usize;
+                let j = (((4.0 * point.y) as isize) & (self.point_count - 1) as isize) as usize;
+                let k = (((4.0 * point.z) as isize) & (self.point_count - 1) as isize) as usize;
+
+                self.ran_float[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+            }
     }
 }
